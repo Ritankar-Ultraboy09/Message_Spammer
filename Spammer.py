b645206539e35a7ef8c354f008ac7048d3ae0a27
Ritankar-Ultraboy09 <78661840+Ritankar-Ultraboy09@users.noreply.github.com>
2021-04-24 16:47:05 +0530

    Add files via upload

---
+++ Spammer.py
@@ -0,0 +1,11 @@
+import pyautogui
+import time
+
+Go = input("Press S ")
+time.sleep(4)
+
+while Go == "S" :
+    pyautogui.typewrite("Spamming Bot working succesfully")
+    pyautogui.press("Enter")
+    time.sleep(1)
+    
